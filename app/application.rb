87a9fa0561b887b076f9485eabf952243e7b4119
Andy Maskiell <maskiella@gmail.com>
2018-02-05 21:08:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.title == item_name}
+      item = @@items.find{|i| i.name == item_name}
     end
   end
 
