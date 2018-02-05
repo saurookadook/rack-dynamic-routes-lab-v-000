8f9022d0308c9ac856e95e7e105585d7df4468c7
Andy Maskiell <maskiella@gmail.com>
2018-02-05 21:09:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
 
-      resp.write item.item_name
+      resp.write item.name
     end
   end
 
