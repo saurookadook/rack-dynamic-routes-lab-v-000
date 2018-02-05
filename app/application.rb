01812780d8bab9d5f11c884da43a559f5561750d
Andy Maskiell <maskiella@gmail.com>
2018-02-05 21:26:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,8 +11,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-
-      binding.pry
+      
       if item
         resp.write item.price
       else
