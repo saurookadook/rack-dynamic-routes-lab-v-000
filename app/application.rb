5645d61e03f20d835f267bf9f760d6db1ae715c3
Andy Maskiell <maskiella@gmail.com>
2018-02-05 21:22:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,8 +17,8 @@ class Application
         resp.status = 400
       end
     else
-      resp.write "Item not found"
-      resp.status = 400
+      resp.write "Route not found"
+      resp.status = 404
     end
 
     resp.finish
